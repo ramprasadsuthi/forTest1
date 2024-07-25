2324ba82a523b45c48ab0557e6e0049dab97779f
ramprasadsuthi <ramprasadsuthi@gmail.com>
2024-07-25 18:22:24 +0530

    First commit

---
+++ src/basic/Test1.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b = 20;
+	    int sum = a+b; 
+	    
+	    System.out.println("Sum of a+b is " + sum);
+	    
+	    // I want to print "Sadik" 
+	    
+	    System.out.print("\"Sadik\"");
+
+	}
+
+}
